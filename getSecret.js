86b0d70ecb90609f9a6e3f4e6f087639b7412a7c
Archariya Pliansaneh <114090987+archariya-007@users.noreply.github.com>
2022-10-28 17:06:15 -0500

    update readme

--- getSecret.js
+++ getSecret.js
@@ -1,6 +1,6 @@
 const victoria = require('./get-secret');
 
-let secretId = 'bc_elfuerte_pubkey-tf';
+let secretId = 'bc_elfuerte_pubkey-tf'; //aws secret manager
 
 var secret = victoria.getSecrets(secretId).then(result=> {
     console.info(result.pubKey);
