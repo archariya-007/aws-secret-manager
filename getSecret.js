e917176119ad7aa67dc2df9965e6690a2e8442d6
Archariya Pliansaneh <114090987+archariya-007@users.noreply.github.com>
2022-10-29 14:48:42 -0500

    handle when secret id not found

--- getSecret.js
+++ getSecret.js
@@ -1,9 +1,12 @@
 const victoria = require('./get-secret');
 
-let secretId = 'bc_elfuerte_pubkey-tf'; //aws secret manager
+let secretId = 'bc_elfuerte_pubkey-tf2'; //aws secret manager
 
-var secret = victoria.getSecrets(secretId).then(result=> {
+victoria.getSecrets(secretId).then(result=> {
     console.info(result.pubKey);
     return result.pubKey;
+}).catch(err => {
+    console.error(err.code);
+    return null;
 });
 
